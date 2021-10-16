55b484c311cd1e08a7b65135d1b448f5e9be2639
Sam Bencivengo <sambencivengo@gmail.com>
2021-10-16 13:47:54 -0400

    Finished

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react';
 
 function Toggle() {
-  return <button>OFF</button>;
+  // is the state we are stting acting on a binary, consider using a boolean.
+  const [isOn, setIsOn] = useState(false);
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+  const color = isOn ? 'red' : 'white';
+  console.log(`The value of isOn is: ${isOn}`);
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? 'ON' : 'OFF'}
+    </button>
+  );
 }
 
 export default Toggle;
